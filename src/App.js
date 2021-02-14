8cc7776bb5dfcb5a92516dafec81903dfcc5ec75
Juan Quispe <jquispeh63@gmail.com>
2021-02-14 16:41:51 -0500

    Adds page control of mangas
    -Adds manga-pages div to hold the buttons
    -Adds page to determine which page of the list show
    -Adds pageSize to determine how many mangas to show (Responsive)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { useState, useEffect, useRef } from 'react';
 import resources from './resources/resources.json';
 import Manga from './Manga'
 
 const App = () => {
   const mangas = resources.data;
+  const [pageSize, setPageSize] = useState(5);
+  const [page, setPage] = useState(0);
+
+  const dashboard = useRef(null);
+  useEffect(() => {
+    const width = dashboard.current.offsetWidth;
+    if(width <= 320) {
+      setPageSize(2);
+    }else if(width <= 640) {
+      setPageSize(4);
+    }else{
+      setPageSize(5);
+    }
+  }, [dashboard])
   return (
     <>
-    {mangas.map((data, index) => {
-      return <Manga key={index} name={data.nombre} url={data.img}/>
-    })}
+    <div className="manga-dashboard" ref={dashboard}>
+      {mangas.slice(page * pageSize, page * pageSize + pageSize).map((data, index) => {
+        return <Manga key={index} name={data.nombre} url={data.img} responsive={pageSize == 5} />
+      })}
+    </div>
+    <div className="manga-pages">
+      {Array.from(Array(Math.ceil(mangas.length / pageSize)), (e, i) => {
+        return <button key={i} onClick={() => setPage(i)}>{i + 1}</button>
+      })}
+    </div>
     </>
   );
 }
--- src/Manga.js
+++ src/Manga.js
@@ -1,10 +1,10 @@
-const Manga = ({name, url}) => {
+const Manga = ({name, url, responsive}) => {
     const src = require(`./resources/${url}.jpg`).default;
 
     return (
         <div className="manga-holder">
             <img className="manga-frame" src={src}></img>
-            <h4>{name}</h4>
+            {responsive ? (<h3>{name}</h3>) : (<h5>{name}</h5>)}
         </div>
     );
 }
